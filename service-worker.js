921d0384f3efca06c10baa95bbec201628b54d9d
trouillard <frank.trouillard@renault.com>
2019-07-01 15:58:29 +0200

    la totale

--- service-worker.js
+++ service-worker.js
@@ -21,11 +21,7 @@ var filesToCache = [
   '/polyfills.js',
   '/runtime.js',
   '/styles.js',
-  '/vendor.js',
-  '/en.json',
-  '/fr.json',
-  'en_GB.json',
-  'fr_FR.json'
+  '/vendor.js'
 ];
 
 self.addEventListener('install', function(e) {
